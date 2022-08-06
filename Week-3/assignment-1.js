1587ff2372a72c50b1f0812f22750fc04fd115b1
soumya213 <39231020+soumya213@users.noreply.github.com>
2022-08-06 14:29:59 +0530

    Assignment-3 Memoize function
    Assignment-3 Memoize function

---
+++ Week-3/assignment-1.js
@@ -0,0 +1,40 @@
+console.log('javscript is linked - memoize Function')
+
+function memoize(fn) {
+    console.log(`memoize function called`);
+    const cache=new Map()
+    return function(...args){
+        const key = args.toString();
+        if (cache.has(key)){
+            cache.get(key)
+        }
+
+        cache.set(key,fn(...args))
+        return cache.get(key)
+    }
+    
+}
+
+function add(...numbers){
+    console.log(`add function called`);
+    let variableSent=[...numbers]
+    sum=0
+    for (i in variableSent){
+        sum=sum+variableSent[i]
+    }
+
+   return sum
+}
+
+function time(fn){
+    console.time()
+    console.log(fn())
+    console.timeEnd();
+}
+
+const memoizeAdd=memoize(add)
+
+time(()=> memoizeAdd(100,100))
+time(()=> memoizeAdd(100))
+time(()=> memoizeAdd(100,200))
+time(()=> memoizeAdd(100,100))
